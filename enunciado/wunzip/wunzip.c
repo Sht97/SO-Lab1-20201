27fe8e83a088fc042e9a84f1a4ab0ab561e16d5b
Daniel Rivera <Danielf.r97@gmail.com>
2020-10-04 21:05:39 -0500

    unzip

--- enunciado/wunzip/wunzip.c
+++ enunciado/wunzip/wunzip.c
@@ -1,56 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX_RLEN 50
- 
-/* Returns the Run Length Encoded string for the 
-   source string src */
-char* encode(char* src)
+#include<stdlib.h>
+
+void unzip(char *nombre);
+FILE *inFile;
+
+int main(int argc, char *argv[])
 {
-    int rLen;
-    char count[MAX_RLEN];
-    int len = strlen(src);
- 
-    /* If all characters in the source string are different, 
-    then size of destination string would be twice of input string.
-    For example if the src is "abcd", then dest would be "a1b1c1d1"
-    For other inputs, size would be less than twice.  */
-    char* dest = (char*)malloc(sizeof(char) * (len * 2 + 1));
- 
-    int i, j = 0, k;
- 
-    /* traverse the input string one by one */
-    for (i = 0; i < len; i++) {
- 
-        /* Copy the first occurrence of the new character */
-        dest[j++] = src[i];
- 
-        /* Count the number of occurrences of the new character */
-        rLen = 1;
-        while (i + 1 < len && src[i] == src[i + 1]) {
-            rLen++;
-            i++;
-        }
- 
-        /* Store rLen in a character array count[] */
-        sprintf(count, "%d", rLen);
- 
-        /* Copy the count[] to destination */
-        for (k = 0; *(count + k); k++, j++) {
-            dest[j] = count[k];
+    if (argc < 2) {
+        printf("wunzip: file1 [file2 ...]\n");
+        return 1;
+    }
+
+    //Para descomprimir multiples archivos
+    for(int i=1; i<argc; i++){
+        unzip(argv[i]);
+    }
+    return 0;
+}
+
+void unzip(char *nombre){
+    char letra;
+    int numero;
+    inFile = fopen(nombre,"r"); //Se intenta abrir el archivo
+    if(inFile == NULL) {
+        exit(1);
+    }
+    while (fread(&numero, 4, 1, inFile)) { //Primero lee la letra 
+        fread(&letra, 1, 1, inFile); //Luego lee el número
+
+        for (int a = 0; a < numero; a++)//Imprime la letra el número de veces
+        {
+            printf("%c", letra);
         }
     }
- 
-    /*terminate the destination string */
-    dest[j] = '\0';
-    return dest;
-}
- 
-/*driver program to test above function */
-int main()
-{
-    char str[] = "aaaaaaaaaabbbb";
-    char* res = encode(str);
-    printf("%s", res);
-    getchar();
-}
+    fclose(inFile);
+    }
